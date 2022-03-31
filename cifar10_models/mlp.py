f3d0c1d674cda10bf4749c87300f3fdc293be922
Yang Xianglin <yxlyzxl@gmail.com>
2022-03-31 15:26:57 +0800

    minor

--- cifar10_models/mlp.py
+++ cifar10_models/mlp.py
@@ -9,17 +9,23 @@ __all__ = ["mlp3"]
 class MLP(nn.Module):
     def __init__(self, hidden_layer):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(28*28, hidden_layer)
-        self.fc2 = nn.Linear(hidden_layer,hidden_layer)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Sequential(
+            nn.Linear(28*28, hidden_layer),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(hidden_layer, hidden_layer),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
         self.fc3 = nn.Linear(hidden_layer,10)
-        self.droput = nn.Dropout(0.2)
         
     def forward(self,x):
-        x = x.view(-1,28*28)
-        x = F.relu(self.fc1(x))
-        x = self.droput(x)
-        x = F.relu(self.fc2(x))
-        x = self.droput(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc3(x)
         return x
 
